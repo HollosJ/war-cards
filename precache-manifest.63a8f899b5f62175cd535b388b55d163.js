27c3e112e0f8d12ee215fe7d2448cda2265c8d24
HollosJ <hollosj99@gmail.com>
2020-09-10 01:34:52 +0100

    Updates

---
+++ precache-manifest.63a8f899b5f62175cd535b388b55d163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329795a9eaa449cd85224de6b120d7e3",
+    "url": "/war-cards/index.html"
+  },
+  {
+    "revision": "459b02ef4f6af3d7385b",
+    "url": "/war-cards/static/css/main.ed88acc1.chunk.css"
+  },
+  {
+    "revision": "8f96e3d0bc8016b584f2",
+    "url": "/war-cards/static/js/2.30032cb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/war-cards/static/js/2.30032cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459b02ef4f6af3d7385b",
+    "url": "/war-cards/static/js/main.b7ea7003.chunk.js"
+  },
+  {
+    "revision": "f6ee982dfe8a8b235582",
+    "url": "/war-cards/static/js/runtime-main.d190daf4.js"
+  }
+]);
