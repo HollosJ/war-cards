faaeae6b654fe9e33a810dc6cebb3f78f275f3bf
HollosJ <hollosj99@gmail.com>
2020-09-20 17:19:55 +0100

    ✨ Added the Cold War

--- src/data.js
+++ src/data.js
@@ -137,6 +137,346 @@ export let data = {
         "https://cdnph.upi.com/svc/sv/upi/8581596809704/2020/1/ffc24f10fb1da77fb975b189d0a7d2f9/UPI-Almanac-for-Sunday-Aug-9-2020.jpg",
     },
   ],
+  Cold_War: [
+    {
+      year: 1947,
+      events: [
+        "March 12 : Truman Doctrine - Truman declares active role in Greek Civil War.",
+        "June : Marshall Plan is announced setting a precedent for helping countries combat poverty, disease and malnutrition.",
+        "September 2 - Rio Pact - U.S. meet 19 Latin American countries and created a security zone around the hemisphere.",
+      ],
+      img:
+        "https://cdn.cfr.org/sites/default/files/styles/full_width_xl/public/image/2012/03/lindsay-truman-doctrine-20120312.jpg",
+    },
+    {
+      year: 1948,
+      events: [
+        "February 25 : Communist takeover in Czechoslovakia.",
+        "March 2: Truman's Loyalty Program created to catch Cold War spies.",
+        "March 17: Brussels Pact organized to protect Europe from communism.",
+        "June 24 : Berlin Blockade begins lasting 11 months.",
+      ],
+      img:
+        "https://images.jacobinmag.com/wp-content/uploads/2018/03/13125136/cz.jpg",
+    },
+    {
+      year: 1949,
+      events: [
+        "April 4 : NATO ratified.",
+        "May 12 : Berlin Blockade ends.",
+        "29 August : Russia tested its first atomic bomb.",
+        "October 1 : Communist Mao Zedong takes control of China and establishes the People's Republic of China.",
+        "December 1 - Chiang Kai-shek moved to Formosa and created Nationalist government.",
+      ],
+      img:
+        "https://www.wired.com/wp-content/uploads/archive/images/article/full/2007/08/joe_one_250x.jpg",
+    },
+    {
+      year: 1950,
+      events: [
+        "January 30 - Truman approved H-bomb development.",
+        "February : Joe McCarthy begins Communist witch hunt and loyalty tests.",
+        "June 24: Korean War begins. Stalin supports North Korea who invade South Korea equipped with Soviet weapons.",
+      ],
+      img:
+        "https://www.atomicheritage.org/sites/default/files/Truman%20orders%20H-Bomb.jpg",
+    },
+    {
+      year: 1951,
+      events: [
+        "January 12 : Federal Civil Defense Administration established.",
+        "April 11 - Truman fires MacArthur.",
+      ],
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/0/0b/Truman_and_MacArthur.JPG",
+    },
+    {
+      year: 1952,
+      events: ["A-bombs developed by Britain."],
+      img:
+        "https://www.aspistrategist.org.au/wp-content/uploads/2018/01/Op_hurricane.jpg",
+    },
+    {
+      year: 1953,
+      events: [
+        "March 17-June 4 Nuclear Arms Race atomic test series of 11 explosions at Nevada Test Site.",
+        'April 15: RAND report on the "Vulnerability of U. S. Strategic Air Power".',
+        "July : Korean War ends.",
+        "December 8: Ike's Atoms for Peace speech.",
+      ],
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Uk-grable2.jpg/200px-Uk-grable2.jpg",
+    },
+    {
+      year: 1954,
+      events: [
+        "March 1: H-bomb Castle-Bravo test.",
+        "March : KGB established.",
+        "CIA helps overthrow unfriendly regimes in Iran and Guatemala.",
+        "July : Vietnam split at 17th parallel.",
+      ],
+      img:
+        "https://www.atomicheritage.org/sites/default/files/styles/large/public/Castle-Bravo%20%28DTRIAC%2022-DPY-1-7%29.jpg",
+    },
+    {
+      year: 1955,
+      events: ["May : Warsaw Pact formed."],
+      img:
+        "https://www.nato.int/nato_static_fl2014/assets/pictures/history_e2-cold-war-def-det-warsaw-pact/20161201_Photo3.jpg",
+    },
+    {
+      year: 1956,
+      events: [
+        "June 29: USSR sent tanks into Poznan, Poland, to suppress demonstrations by workers.",
+        "September 4: USSR sent military aid to Afghanistan.",
+        "October - November : Rebellion put down in Communist Hungary.",
+        "October 29: Suez Crisis began with Israeli attack led by Moshe Dayan against Egyptian forces in the Sinai.",
+        "Egypt took control of Suez Canal.",
+      ],
+      img: "https://www.videofact.com/polska/robocze%20today/poznan1B.jpg",
+    },
+    {
+      year: 1957,
+      events: [
+        "August 26: Vostok rocket launched 1st ICBM.",
+        "October 4 : Sputnik launched into orbit.",
+        "November 3: Sputnik II launched - Laika died in space.",
+      ],
+      img:
+        "https://media4.s-nbcnews.com/j/msnbc/Components/Slideshows/_production/ss_071004_sputnik50/ss_071004_sputnik50_11.grid-6x2.jpg",
+    },
+    {
+      year: 1958,
+      events: [
+        "January 31: Explorer I launched.",
+        "July : NASA began Mercury project using Atlas rocket.",
+        "November : Khrushchev demands withdrawal of troops from Berlin.",
+      ],
+      img: "https://live.staticflickr.com/3945/15523335679_029a947ed1_z.jpg",
+    },
+    {
+      year: 1959,
+      events: [
+        "January : Cuba taken over by Fidel Castro.",
+        "September : Khrushchev visits United States; The Kitchen Debate.",
+      ],
+      img:
+        "https://www.outlookindia.com/outlooktraveller/resizer.php?src=https://www.outlookindia.com/outlooktraveller/public/uploads/articles/travelnews/Hot-Dog-Bronx-Zoo.jpg&w=200&h=300",
+    },
+    {
+      year: 1960,
+      events: [
+        "A-bombs developed by France.",
+        "May : Soviet Union reveals that U.S. spy plane was shot down over Soviet territory.",
+        "November : John F. Kennedy elected President of USA.",
+        "December 19: Cuba openly aligns itself with the Soviet Union and their policies.",
+      ],
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/41x-HdDMaML._AC_.jpg",
+    },
+    {
+      year: 1961,
+      events: [
+        "April : Bay of Pigs invasion see Cuban Missile Crisis Timeline.",
+        "August 13 : Berlin border is closed.",
+        "August 17 : Construction of Berlin Wall begins.",
+      ],
+      img: "https://ichef.bbci.co.uk/images/ic/320xn/p03hbc08.jpg",
+    },
+    {
+      year: 1962,
+      events: [
+        "U.S. involvement in Vietnam increases see Vietnam War Timeline.",
+        "October : Cuban Missile Crisis see Cuban Missile Crisis Timeline.",
+      ],
+      img:
+        "https://www.history.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTU3ODc5MDg1MzYwMTYyNTI3/distances-of-major-cites-from-cuba.jpg",
+    },
+    {
+      year: 1963,
+      events: [
+        "July : Nuclear Test Ban Treaty ratified.",
+        "November : President Kennedy assassinated in Dallas, Texas.",
+      ],
+      img:
+        "https://media1.s-nbcnews.com/j/newscms/2017_43/2199586/171023-jfk-motorocade-dallas-se-706p_108cd216604ef11ff135c3238a7a55e2.fit-760w.jpg",
+    },
+    {
+      year: 1964,
+      events: [
+        "August : Gulf of Tonkin incident.",
+        "October: A-bombs developed by China.",
+      ],
+      img:
+        "https://cdn1.i-scmp.com/sites/default/files/styles/768x768/public/2014/10/17/80953b709aa76e2f46e7801dfb668b53.jpg",
+    },
+    {
+      year: 1965,
+      events: [
+        "April : U.S. Marines sent to Dominican Republic to fight Communism.",
+        "July : Announcement of dispatching of 200,000 U.S. troops to Vietnam.",
+      ],
+      img:
+        "https://nsarchive2.gwu.edu/NSAEBB/NSAEBB513/photos/Dominican-Crisis-1-575.jpg",
+    },
+    {
+      year: 1966,
+      events: ["B-52s Bomb North Vietnam."],
+      img: "https://www.historycentral.com/Vietnam/B-52.jpg",
+    },
+    {
+      year: 1967,
+      events: [
+        "The US Secretary of Defence Robert McNamara admits that the US bombing raids had failed to meet their objectives.",
+      ],
+      img:
+        "https://static01.nyt.com/images/2016/02/08/blogs/08iht-oldfeb08-veteran/08iht-oldfeb08-veteran-blog480.jpg",
+    },
+    {
+      year: 1968,
+      events: [
+        "January : North Korea captured U.S.S. Pueblo.",
+        "President Johnson does not run for the presidency and Richard Nixon Elected President of the USA.",
+        "August : Soviet Red Army crush Czechoslovakian revolt.",
+      ],
+      img:
+        "https://s3.amazonaws.com/static.history.state.gov/milestones/prague.jpg",
+    },
+    {
+      year: 1969,
+      events: ["July 20 : Apollo 11 lands on the moon."],
+      img: "https://live.staticflickr.com/2840/8980505187_74eb03d8b4_c.jpg",
+    },
+    {
+      year: 1970,
+      events: ["April : President Nixon extends Vietnam War to Cambodia."],
+      img:
+        "https://www.theamericanconservative.com/wp-content/uploads/2019/04/nixon-cambodia.jpeg",
+    },
+    {
+      year: 1971,
+      events: ["Publication of the Pentagon Papers."],
+      img: "https://nsarchive2.gwu.edu/NSAEBB/NSAEBB348/nytfrontpg.jpg",
+    },
+    {
+      year: 1972,
+      events: [
+        "February: President Richard Nixon visits China.",
+        "July : SALT I signed.",
+      ],
+      img:
+        "https://thediplomat.com/wp-content/uploads/2013/11/sizes/td-story-s-2/Nixon_Mao_1972-02-29.jpg",
+    },
+    {
+      year: 1973,
+      events: [
+        "January : Cease fire in Vietnam between North Vietnam and United States.",
+        "September : U.S. supported coup overthrows Chilean government.",
+        "October : Egypt and Syria attack Israel; Egypt requests Soviet aid.",
+      ],
+      img:
+        "https://i.guim.co.uk/img/media/75c16c61ecf9120515bfa40178acbb54bbaa7b25/68_242_3594_2158/master/3594.jpg?width=700&quality=85&auto=format&fit=max&s=23cc45532c4eab72513b86bb15f5b608",
+    },
+    {
+      year: 1974,
+      events: ["August : President Nixon resigns."],
+      img: "https://i.ytimg.com/vi/lzXL7C0JQDM/hqdefault.jpg",
+    },
+    {
+      year: 1975,
+      events: [
+        "April 17 : North Vietnam defeats South Vietnam which falls to Communist forces.",
+      ],
+      img:
+        "https://static01.nyt.com/images/2012/04/29/learning/Apr30LN/Apr30LN-blog480.jpg",
+    },
+    {
+      year: 1976,
+      events: [
+        "February: Soviet and Cuban forces help to install Communist government in Angola.",
+      ],
+      img:
+        "https://thetorohistoricalreview.files.wordpress.com/2019/01/the-sun-of-freedom-over-angola-a_kinsbursky-1976-2.jpg?w=400",
+    },
+    {
+      year: 1979,
+      events: [
+        "January: U.S. and China establish diplomatic relations.",
+        "July : SALT II signed.",
+        "November : Shah of Iran overthrown; Iranian Hostage Crisis.",
+        "December: Soviet forces invade Afghanistan.",
+      ],
+      img:
+        "https://cdn.theatlantic.com/thumbor/M2jyUG3OLNMYhltexO4yeiysjuI=/5x60:3300x1913/720x405/media/img/mt/2019/01/RTR238JU/original.jpg",
+    },
+    {
+      year: 1980,
+      events: [
+        "August: Polish shipyard workers strike Solidarity Union formed. Strike leader Lech Walesa is elected as the head of Solidarity.",
+      ],
+      img: "https://libcom.org/files/Strike_Gdansk_1980[1].jpg",
+    },
+    {
+      year: 1983,
+      events: [
+        "President Reagan proposes Strategic Defence Initiative.",
+        "October : U.S. troops invades and overthrows regime in Grenada.",
+      ],
+      img: "https://libcom.org/files/images/history/GrenadaInvasion1983.jpg",
+    },
+    {
+      year: 1985,
+      events: [
+        'Mikhail Gorbachev becomes leader of the Soviet Union initiating a campaign of openness called "glasnost" and restructuring called "perestroika"',
+      ],
+      img:
+        "https://cdn.britannica.com/19/11419-004-F2167F3B/Mikhail-Gorbachev-1985.jpg",
+    },
+    {
+      year: 1986,
+      events: [
+        "October : President Reagan and Gorbachev resolve to remove all intermediate nuclear missiles from Europe.",
+      ],
+      img:
+        "https://cdn.britannica.com/s:500x500/79/193879-050-18001534/Pres-Mikhail-Gorbachev-Ronald-Reagan-Soviet-Reykjavik-October-1986.jpg",
+    },
+    {
+      year: 1987,
+      events: [
+        "October : Reagan and Gorbachev agree to remove all medium and short-range nuclear missiles.",
+      ],
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/2/25/Gorbachev_and_Reagan_1987-9.jpg",
+    },
+    {
+      year: 1989,
+      events: [
+        "January : Soviet troops withdraw from Afghanistan.",
+        "June : Poland becomes independent.",
+        "September : Hungary becomes independent.",
+        "November : Berlin Wall is demolished and East Germany allows unrestricted migration to West Germany.",
+        "December : Communist governments fall in Czechoslovakia, Bulgaria, and Romania - The decline of the Soviet empire.",
+      ],
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/1/1c/West_and_East_Germans_at_the_Brandenburg_Gate_in_1989.jpg",
+    },
+    {
+      year: 1990,
+      events: [
+        "March : Lithuania becomes independent.",
+        "May 29 : Boris Yeltsin elected as President of Russia.",
+        "October 3 : Germany reunited.",
+      ],
+      img:
+        "https://usrussiarelations.org/media/e87349001ed34a4dba4a1f33cc890b90_900.jpg",
+    },
+    {
+      year: 1991,
+      events: ["August : End of Soviet Union and the Cold War Ends."],
+      img:
+        "https://static01.nyt.com/images/2017/08/28/opinion/28westad-redcenturyWeb/28westad-redcenturyWeb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    },
+  ],
   The_Falklands_War: [
     {
       year: "April",
